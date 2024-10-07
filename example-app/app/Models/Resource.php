bf5f170f13ce4ddde9de37b417dfb5cf491c5755
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2024-10-07 11:34:52 +0300

    Revert "update"
    This reverts commit 70efcf760d2a0f85cef12ddf001b119bca51149a.

--- example-app/app/Models/Resource.php
+++ example-app/app/Models/Resource.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tree extends Model
+class Resource extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['name'];
 }
--- example-app/database/migrations/2024_09_13_094949_create_resources_table.php
+++ example-app/database/migrations/2024_09_13_094949_create_resources_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTreesTable extends Migration
+class CreateResourcesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CreateTreesTable extends Migration
      */
     public function up()
     {
-        Schema::create('trees', function (Blueprint $table) {
+        Schema::create('resources', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('status'); // New column added
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ class CreateTreesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('trees');
+        Schema::dropIfExists('resources');
     }
 }
